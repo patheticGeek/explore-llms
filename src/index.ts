11e3823327c4ed89de84f540304c69db108d68e8
Pathetic Geek <geekpathetic@gmail.com>
2024-03-01 01:13:56 +0530

    something works even if it takes eternity

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,76 @@
 import { ChatOllama } from "@langchain/community/chat_models/ollama";
-import { StringOutputParser } from "@langchain/core/output_parsers";
-import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { OllamaEmbeddings } from "@langchain/community/embeddings/ollama";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import ms from "ms";
+import fs from "node:fs/promises";
+import prompts from "prompts";
 
 const main = async () => {
+  const prompt = (
+    await prompts({
+      type: "text",
+      name: "value",
+      message: "What do you want?",
+    })
+  ).value;
+
+  const start = performance.now();
+
+  const data = await fs.readFile("./dataset.json");
+
+  const docs = JSON.parse(data.toString());
+
   const chatModel = new ChatOllama({
-    baseUrl: "http://localhost:11434", // Default value
+    baseUrl: "http://localhost:11434",
     model: "mistral",
   });
 
-  // const result = await chatModel.invoke("what is LangSmith?");
-  // console.log("result", result);
-
-  const outputParser = new StringOutputParser();
-
-  const prompt = ChatPromptTemplate.fromMessages([
-    ["system", "You are a world class technical documentation writer."],
-    ["user", "{input}"],
-  ]);
-  // const chain = prompt.pipe(chatModel);
-  const llmChain = prompt.pipe(chatModel).pipe(outputParser);
-
-  const result = await llmChain.invoke({
-    input: "what is LangSmith?",
+  const embeddings = new OllamaEmbeddings({
+    model: "mistral",
+    baseUrl: "http://localhost:11434",
   });
-  console.log("main -> result:", result);
+
+  console.log(`[prompt] ${prompt}`);
+  const vectorStore = await MemoryVectorStore.fromTexts(docs, [], embeddings);
+  console.log("vector store generated", `${performance.now() - start}ms`);
+
+  const vectorResults = await vectorStore.similaritySearch(prompt, 2);
+  console.log(
+    "vectorResults",
+    ms(performance.now() - start),
+    "vectorResults.length",
+    vectorResults.length
+  );
+
+  const result = await chatModel.invoke(`
+You are Pathetic Geek's assistant which is built to help users find resources from his bookmarks.
+
+The user wants a website as follows:
+${prompt}
+
+The resources that match the prompt are described as follows, each inside <resource></resource> tags:
+${vectorResults.map((r) => `<resource>${r.pageContent}</resource>`)}
+
+What are the best websites for the user to checkout? Give a list of 5 websites along with a very short description:
+  `);
+
+  console.log("result", ms(performance.now() - start));
+  console.log("result.content", result.content);
+
+  // const result = await chatModel.invoke("what is LangSmith?");
+  // const outputParser = new StringOutputParser();
+
+  // const prompt = ChatPromptTemplate.fromMessages([
+  //   ["system", "You are a world class technical documentation writer."],
+  //   ["user", "{input}"],
+  // ]);
+  // // const chain = prompt.pipe(chatModel);
+  // const llmChain = prompt.pipe(chatModel).pipe(outputParser);
+
+  // const result = await llmChain.invoke({
+  //   input: "what is LangSmith?",
+  // });
+  // console.log("main -> result:", result);
 
   return "Done...";
 };
