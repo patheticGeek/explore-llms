6b4d9bdbdbc6bf5583678bce252fb5473d21e943
Pathetic Geek <geekpathetic@gmail.com>
2024-03-02 20:03:20 +0530

    just saving

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { ChatOllama } from "@langchain/community/chat_models/ollama";
 import { OllamaEmbeddings } from "@langchain/community/embeddings/ollama";
+import { config } from "dotenv";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import ms from "ms";
 import fs from "node:fs/promises";
 import prompts from "prompts";
+
+config();
 
 const main = async () => {
   const prompt = (
@@ -13,6 +16,8 @@ const main = async () => {
       message: "What resource are you looking for?",
     })
   ).value;
+
+  if (!prompt) throw new Error("Provide a prompt");
 
   const start = performance.now();
 
@@ -28,13 +33,18 @@ const main = async () => {
   const embeddings = new OllamaEmbeddings({
     model: "mistral",
     baseUrl: "http://localhost:11434",
+    requestOptions: {
+      useMMap: true,
+      numThread: 6,
+      numGpu: 1,
+    },
   });
 
-  console.log(`[prompt] ${prompt}`);
+  console.log(`[prompt] ${prompt}\n`);
   const vectorStore = await MemoryVectorStore.fromTexts(docs, [], embeddings);
-  console.log("vector store generated", `${performance.now() - start}ms`);
+  console.log("vector store generated", ms(performance.now() - start));
 
-  const vectorResults = await vectorStore.similaritySearch(prompt, 2);
+  const vectorResults = await vectorStore.similaritySearch(prompt, 10);
   console.log(
     "vectorResults",
     ms(performance.now() - start),
@@ -48,13 +58,13 @@ You are Pathetic Geek's assistant which is built to help users find resources fr
 The user wants a website as follows:
 ${prompt}
 
-The resources that match the prompt are described as follows, each inside <resource></resource> tags:
-${vectorResults.map((r) => `<resource>${r.pageContent}</resource>`)}
+The resources that match the prompt are described as follows:
+${vectorResults.map((r) => `- ${r.pageContent}`).join("\n")}
 
-What are the best websites for the user to checkout? Give a list of 5 websites along with a very short description:
+What are the best websites for the user to checkout? Give a list of 5 websites from the above list only along with a short description:
   `);
 
-  console.log("result", ms(performance.now() - start));
+  console.log("result generated in", ms(performance.now() - start));
   console.log("result.content", result.content);
 
   // const result = await chatModel.invoke("what is LangSmith?");
