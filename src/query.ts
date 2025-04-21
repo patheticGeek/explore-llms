519c50cf85e9d29a20ee614e1ca836b633ef4acc
Pathetic Geek <geekpathetic@gmail.com>
2025-04-21 05:50:07 +0530

    query using vector db function call with gemini

--- src/query.ts
+++ src/query.ts
@@ -1,10 +1,49 @@
-import { ChatCohere } from "@langchain/cohere";
+import { ContentListUnion, GenerateContentResponse, Type } from "@google/genai";
+import { Index } from "@upstash/vector";
 import { config } from "dotenv";
 import ms from "ms";
 import prompts from "prompts";
-import { getVectorStore } from "./lib/vectorStore";
+import getAi from "./lib/ai";
 
 config();
+
+const ai = getAi();
+
+const queryFunctionDeclaration = {
+  name: "query_vector_db",
+  description: "Queries the vector db in which the bookmarks are stored.",
+  parameters: {
+    type: Type.OBJECT,
+    properties: {
+      query: {
+        type: Type.STRING,
+        description: "The string that needs to be queried in db",
+      },
+      noOfDocuments: {
+        type: Type.NUMBER,
+        description: "The number of documents to get (optional, default: 10)",
+      },
+    },
+    required: ["query"],
+  },
+};
+
+const aiConfig = {
+  tools: [{ functionDeclarations: [queryFunctionDeclaration] }],
+};
+
+const getPrompt = (prompt: string) =>
+  `
+You are Pathetic Geek's assistant which is built to help people find resources from his bookmarks.
+Try to give as many options as possible but try to give only the ones that match the query the most. It is not necessary to return all results asked of you.
+If there are less than 2 try to modify query and find more 2 times max.
+Do not answer any unrelated query's or vulgar ones.
+
+The user wants a website as follows:
+${prompt}
+
+What are the best websites for the user to checkout? Give a list of websites from the above list only. Also give the url and a less than 2 line description:
+    `.trim();
 
 const main = async () => {
   const prompt = (
@@ -20,39 +59,60 @@ const main = async () => {
 
   const start = performance.now();
 
-  const { vectorStore } = getVectorStore();
-  const vectorResults = await vectorStore.similaritySearch(prompt, 10);
-  console.log("vectorResults", ms(performance.now() - start));
-
-  const chatModel = new ChatCohere({
-    temperature: 0,
-    maxRetries: 1,
+  const index = new Index({
+    url: process.env.UPSTASH_VECTOR_REST_URL,
+    token: process.env.UPSTASH_VECTOR_REST_TOKEN,
   });
 
-  const chatMessage = `
-You are Pathetic Geek's assistant which is built to help people find resources from his bookmarks.
+  const contents: ContentListUnion = [
+    { role: "user", parts: [{ text: getPrompt(prompt) }] },
+  ];
 
-The user wants a website as follows:
-${prompt}
+  let haveAnswer = false;
+  let response: GenerateContentResponse | undefined;
 
-The bookmarks that match the prompt are as follows:
-${
-  vectorResults.length
-    ? vectorResults
-        .map(
-          ({ pageContent, metadata }) =>
-            `URL: ${metadata.url}\nName:${metadata.name}\nFolder: ${metadata.folder}\nSummary: ${pageContent}`
-        )
-        .join("\n")
-    : "No websites found using the query"
-}
+  while (!haveAnswer) {
+    response = await ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: contents,
+      config: aiConfig,
+    });
 
-What are the best websites for the user to checkout? Give a list of 4 websites from the above list only. Also give the url and a short description:
-    `.trim();
-  const result = await chatModel.invoke(chatMessage);
+    if (response.functionCalls?.length) {
+      console.log("Get function call in response", response.functionCalls);
+      const responses = await Promise.all(
+        response.functionCalls.map(async (functionCall) => {
+          if (
+            functionCall.name === "query_vector_db" &&
+            functionCall.args?.query
+          ) {
+            const output = await index.query({
+              data: functionCall.args?.query as string,
+              topK: functionCall.args?.noOfDocuments
+                ? parseInt(functionCall.args.noOfDocuments as string, 10)
+                : 10,
+              includeVectors: true,
+              includeMetadata: true,
+            });
+            return { name: functionCall.name, response: { output } };
+          }
+        })
+      );
+
+      response.functionCalls.forEach((call, idx) => {
+        contents.push({ role: "model", parts: [{ functionCall: call }] });
+        contents.push({
+          role: "user",
+          parts: [{ functionResponse: responses[idx]! }],
+        });
+      });
+    } else {
+      haveAnswer = true;
+    }
+  }
 
   console.log("result generated in", ms(performance.now() - start));
-  console.log(`Got output ${result}`);
+  console.log(response?.text);
 
   return "Done...";
 };
